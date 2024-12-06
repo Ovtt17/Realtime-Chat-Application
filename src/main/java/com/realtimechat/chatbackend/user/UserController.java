ed0abde13f0b3072cef3bbad44a29a6b100d7c3c
Ovett Mora <ovettmora17@gmail.com>
2024-12-05 21:31:49 -0600

    Add disconnectUser endpoint and findConnectedUsers method to UserController

--- src/main/java/com/realtimechat/chatbackend/user/UserController.java
+++ src/main/java/com/realtimechat/chatbackend/user/UserController.java
@@ -1,10 +1,14 @@
 package com.realtimechat.chatbackend.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,5 +22,19 @@ public class UserController {
     ) {
         userService.saveUser(user);
         return user;
+    }
+
+    @MessageMapping("/user.disconnectUser")
+    @SendTo("/user/topic")
+    public User disconnect(
+            @Payload User user
+    ) {
+        userService.disconnect(user);
+        return user;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> findConnectedUsers() {
+        return ResponseEntity.ok(userService.findConnectedUsers());
     }
 }
